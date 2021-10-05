fb03b5eed7c26bebb678bb20859ba814409d2bab
saman keighobadi <saman.keighobadi@gmail.com>
2021-10-05 20:40:16 +0330

    install dotenv

--- app.js
+++ app.js
@@ -15,4 +15,16 @@ app.post("/api/posts", (req, res) => {
   });
 });
 
+app.post('/api/login',(req,res) => {
+    const user = {
+        id:1,username:'Saman',
+        email:'saman.keighobadi@gmail.com'
+    }
+
+    jwt.sign({user:user},'secret_Key',(err,token) => {
+        
+    })
+
+})
+
 app.listen(5000, () => console.log("server run "));
