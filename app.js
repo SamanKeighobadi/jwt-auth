34e63fbe2f6ece09be83bd0e13c21ad22354b1eb
saman keighobadi <saman.keighobadi@gmail.com>
2021-10-05 20:49:54 +0330

    use env file

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 
+const dotEnv = require("dotenv").config({ path: "./config/.env" });
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
 const app = express();
 
 app.get("/api", (req, res) => {
@@ -15,16 +19,19 @@ app.post("/api/posts", (req, res) => {
   });
 });
 
-app.post('/api/login',(req,res) => {
-    const user = {
-        id:1,username:'Saman',
-        email:'saman.keighobadi@gmail.com'
-    }
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "Saman",
+    email: "saman.keighobadi@gmail.com",
+  };
 
-    jwt.sign({user:user},'secret_Key',(err,token) => {
-        
-    })
+  jwt.sign({ user: user }, SECRET_KEY, (err, token) => {
+    if (err) throw err;
+    res.json({
+      token,
+    });
+  });
+});
 
-})
-
-app.listen(5000, () => console.log("server run "));
+app.listen(5000, () => console.log(`server run at port ${process.env.PORT}`));
